83218131a012df60eb9f0ff2152d45b60a1212ad
Artur Gomes <artur.o.gomes@gmail.com>
2024-10-28 19:02:48 -0400

    feat: :lipstick: App component styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { useState } from 'react';
 import Card from './components/Card'
 import Form from './components/Form'
 import NavBar from './components/NavBar'
+import './App.css';
 
 function App() {
-
+  const [formOrCard, setFormOrCard] = useState<boolean>(false)
   return (
     <>
       <NavBar/>
-      <Card/>
-      <Form />
+      <div className="app__wrapper">
+
+      <button className="button" onClick={() => setFormOrCard(formOrCard => !formOrCard)}>Switch to {formOrCard ? "Form" : "Card"}</button>
+      {formOrCard ? <Card/> : <Form />}
+      </div>
     </>
   )
 }
