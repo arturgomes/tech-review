60cc3fd01451564b61df78265a102e7b165b81e7
Artur Gomes <artur.o.gomes@gmail.com>
2024-10-28 19:18:15 -0400

    feat: add a log for current view when it changes using useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
-import { useState } from 'react';
-import Card from './components/Card'
-import Form from './components/Form'
-import NavBar from './components/NavBar'
-import './App.css';
+import { useState, useEffect } from "react";
+import Card from "./components/Card";
+import Form from "./components/Form";
+import NavBar from "./components/NavBar";
+import "./App.css";
 
 function App() {
-  const [formOrCard, setFormOrCard] = useState<boolean>(false)
+  /**
+   * We'll use useState to manage the form and card view toggle, as well as
+   * useEffect to log which view is currently displayed.
+   */
+  const [formOrCard, setFormOrCard] = useState<boolean>(false);
+
+  // Log the current view when it changes
+  useEffect(() => {
+    console.log(formOrCard ? "Form view displayed" : "Card view displayed");
+  }, [formOrCard]); // Dependency array to log only when formOrCard changes
+
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <div className="app__wrapper">
-
-      <button className="button" onClick={() => setFormOrCard(formOrCard => !formOrCard)}>Switch to {formOrCard ? "Form" : "Card"}</button>
-      {formOrCard ? <Card/> : <Form />}
+        <button className="button" onClick={() => setFormOrCard(!formOrCard)}>
+          Switch to {formOrCard ? "Card" : "Form"}
+        </button>
+        {formOrCard ? ( <Form /> ) : ( <Card /> )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
