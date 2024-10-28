d06c46cd74716306715008e732ceaa098e3e4268
Artur Gomes <artur.o.gomes@gmail.com>
2024-10-28 19:19:01 -0400

    feat: :sparkles: add props to Card component

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,15 @@ function App() {
         <button className="button" onClick={() => setFormOrCard(!formOrCard)}>
           Switch to {formOrCard ? "Card" : "Form"}
         </button>
-        {formOrCard ? ( <Form /> ) : ( <Card /> )}
+        {formOrCard ? (
+          <Form />
+        ) : (
+          <Card
+            title="Dynamic Card Title"
+            description="This description is passed from the App component."
+            img="https://images.unsplash.com/photo-1709884735626-63e92727d8b6?q=80&w=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+        )}
       </div>
     </>
   );
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,27 +1,30 @@
 import React from "react";
 import Button from "../Button";
-
 import "./styles.css";
 
-const Card: React.FC = () => {
+interface CardProps {
+  title: string;
+  description: string;
+  img:string;
+}
+
+const Card: React.FC<CardProps> = ({ title, description, img }) => {
   return (
     <div className="card__wrapper">
-    <div className="card">
-      <img
-        src="https://images.unsplash.com/photo-1709884735626-63e92727d8b6?q=80&w=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Sample Image"
-        className="card__image"
-      />
-      <div className="card__container">
-        <h2 className="card__title">Card Title</h2>
-        <p className="card__description">
-          This is a description for the card content.
-        </p>
-        <div className="card__footer">
-          <Button />
+      <div className="card">
+        <img
+          src={img}
+          alt="Sample Image"
+          className="card__image"
+        />
+        <div className="card__container">
+          <h2 className="card__title">{title}</h2>
+          <p className="card__description">{description}</p>
+          <div className="card__footer">
+            <Button />
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
