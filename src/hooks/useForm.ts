2030dbaf93d6d6a5940ecf0a9f1497bc3236db96
Artur Gomes <artur.o.gomes@gmail.com>
2024-10-28 19:27:48 -0400

    refactor: add return type of useForm

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,6 +1,27 @@
 import { useState } from 'react';
 
-const useForm = (initialValues: { [key: string]: string }) => {
+interface UseFormReturn {
+  values: {
+      [key: string]: string;
+  };
+  errors: {
+      [key: string]: boolean;
+  };
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  validate: () => boolean;
+  clearForm: () => void;
+}
+
+/**
+ * useForm Hook:
+ * @param records of key value pairs to the form 
+ * @returns values and errors: Manages the form values and errors as single state objects.
+ * @returns handleChange: Updates field values and clears specific errors if the user corrects them.
+ * @returns validate: Validates all fields, setting errors for empty fields and returning a boolean indicating if the form is valid.
+ * @returns clearForm: Resets form fields to their initial values after submission.
+ *  
+ */
+const useForm = (initialValues: { [key: string]: string }): UseFormReturn => {
   const [values, setValues] = useState(initialValues);
   const [errors, setErrors] = useState<{ [key: string]: boolean }>({});
 
